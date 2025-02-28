752175046047c6e15a1e42cc3f1bff4b71e81bdd
Saksham Saini <145568181+sakshamsain@users.noreply.github.com>
2025-02-28 16:43:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="Saksham saini",
+    author_email="sakshamsainisaini94@gmail.com",
     packages=find_packages()
 )
